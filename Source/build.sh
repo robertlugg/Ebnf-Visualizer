829167f03bef3e83b7eb529d0c96c86d8f3f18f0
Bob Zhao <ybzhao1989@gmail.com>
2023-01-30 18:14:52 +0800

    switch to winexe, no console

--- Source/build.sh
+++ Source/build.sh
@@ -2,7 +2,7 @@
 
 which mono-csc &>/dev/null || apt-get install -y mono-devel
 
-mono-csc  /t:exe -out:EBNF-Visualizer.exe -reference:/usr/lib/mono/4.5/System.Drawing.dll,/usr/lib/mono/4.5/System.Windows.Forms.dll Scanner.cs Parser.cs Graph.cs EbnfForm.cs
+mono-csc  /t:winexe -out:EBNF-Visualizer.exe -reference:/usr/lib/mono/4.5/System.Drawing.dll,/usr/lib/mono/4.5/System.Windows.Forms.dll Scanner.cs Parser.cs Graph.cs EbnfForm.cs
 
 echo
 echo "Run \`mono EBNF-Visualizer.exe\` to start the app."
